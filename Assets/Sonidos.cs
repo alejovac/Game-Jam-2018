4a216122d98bf288491ac1a4f7bddc0811211fa4
alejovac <alejovac@gmail.com>
2018-01-27 23:53:54 -0300

    Musica fase 1

---
+++ Assets/Sonidos.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sonidos : MonoBehaviour
+{
+	public AudioClip hola;
+	public AudioClip chau;
+	public bool libre;
+
+	void Start()
+	{
+		libre = true;
+	}
+
+	void Update ()
+	{
+		if (libre)
+		{
+			if (Input.GetKeyDown("z"))
+			{
+				AudioSource.PlayClipAtPoint(hola, transform.position);
+				libre = false;
+			}
+		}
+	}
+}
