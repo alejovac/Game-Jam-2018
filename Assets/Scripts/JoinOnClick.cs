c89cb5b4c550ba9d6a7cf7c7881750b941eabcd0
Hennico <ottaku.niko@gmail.com>
2018-01-27 23:30:57 -0300

    Agregado de menu copado
    Se agrego el menu  que hizo mariano

---
+++ Assets/Scripts/JoinOnClick.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class JoinOnClick : MonoBehaviour
+{
+
+    public void Join()
+    {
+        NetworkManager.singleton.StartClient();
+    }
+}
