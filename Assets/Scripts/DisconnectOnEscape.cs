d9a28c318cc4331027bb2e25ed3b25455273f11d
postar <mpocztaruk_92@hotmail.com>
2018-01-28 08:40:19 -0300

    aa
    aa

---
+++ Assets/Scripts/DisconnectOnEscape.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+
+public class DisconnectOnEscape : MonoBehaviour {
+
+        void Update()
+        {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            NetworkManager.singleton.StopHost();
+    }
+}
