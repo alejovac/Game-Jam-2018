0a653b60b5d193b3fd17be46eea8ae5a6690bca5
alejovac <alejovac@gmail.com>
2018-01-28 12:12:05 -0300

    Cometido

---
+++ Assets/ColaScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ColaScript : NetworkBehaviour {
+	public NetworkIdentity ID;
+
+	// Use this for initialization
+	void Start (AudioClip sound) {
+		AudioSource.PlayClipAtPoint (sound, transform.position);
+	}
+}
